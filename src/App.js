14155cd0ba65d3985499d5e928b7abb133e85b62
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-03-02 09:39:59 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client'
+import { useEffect, useState } from 'react'
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+})
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          query GetRates {
+            rates(currency: "USD") {
+              currency
+            }
+          }
+        `,
+      })
+      .then((result) => {
+        // console.log(result)
+        setData(result.data.rates)
+      })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Currencies</h1>
+      <div className='App'>
+        {data.map((rate, i) => (
+          <p key={i}>{rate.currency}</p>
+        ))}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
